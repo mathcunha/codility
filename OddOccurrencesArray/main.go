9571922ed194a9d06ba33a1525cc34b1e7ad6052
Matheus Cunha <mathcunha@gmail.com>
2016-01-05 17:17:59 +0000

    other approach

--- OddOccurrencesArray/main.go
+++ OddOccurrencesArray/main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+func solution(A []int) int {
+	hash := make(map[int]bool)
+	for _, v := range A {
+		_, has := hash[v]
+		if has {
+			hash[v] = true
+		} else {
+			hash[v] = false
+		}
+	}
+	for k, v := range hash {
+		if !v {
+			return k
+		}
+	}
+	return -1
+}
+
 func Solution(A []int) int {
 	// write your code in Go 1.4
 	max := len(A)
@@ -34,8 +52,8 @@ outter:
 	return -1
 }
 func main() {
-	fmt.Printf("the odd ocurrence 7 is %d\n", Solution([]int{9, 3, 9, 3, 9, 7, 9}))
-	fmt.Printf("the odd ocurrence 1 is %d\n", Solution([]int{9, 3, 9, 3, 9, 7, 9, 7, 1}))
-	fmt.Printf("the odd ocurrence 2 is %d\n", Solution([]int{2, 7, 9, 3, 9, 3, 9, 7, 9}))
-	fmt.Printf("the odd ocurrence 5 is %d\n", Solution([]int{9, 3, 9, 7, 5, 3, 9, 7, 9}))
+	fmt.Printf("the odd ocurrence 7 is %d\n", solution([]int{9, 3, 9, 3, 9, 7, 9}))
+	fmt.Printf("the odd ocurrence 1 is %d\n", solution([]int{9, 3, 9, 3, 9, 7, 9, 7, 1}))
+	fmt.Printf("the odd ocurrence 2 is %d\n", solution([]int{2, 7, 9, 3, 9, 3, 9, 7, 9}))
+	fmt.Printf("the odd ocurrence 5 is %d\n", solution([]int{9, 3, 9, 7, 5, 3, 9, 7, 9}))
 }
